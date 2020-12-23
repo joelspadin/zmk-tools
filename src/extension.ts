57fd49f3195d1b2eab0bbded91dfa4fc8bf991a5
Joel Spadin <joelspadin@gmail.com>
2020-12-23 13:05:06 -0600

    Check for wrong .keymap association

--- src/extension.ts
+++ src/extension.ts
@@ -1,10 +1,13 @@
 import * as vscode from 'vscode';
 import { KeymapAnalyzer } from './KeymapAnalyzer';
 import { KeymapParser } from './Parser';
+import { checkForIncorrectFileAssociations } from './settings';
 
 export async function activate(context: vscode.ExtensionContext) {
     const parser = await KeymapParser.init();
     context.subscriptions.push(parser, new KeymapAnalyzer(parser));
+
+    checkForIncorrectFileAssociations();
 }
 
 export function deactivate() {}
